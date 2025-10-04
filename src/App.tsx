f2ce596a001f1cbcfe6d1adcf5b414ecf4402b82
Theguardians58 <125973002+Theguardians58@users.noreply.github.com>
2025-10-04 12:07:45 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
@@ -14,33 +15,64 @@ import Contact from "./pages/Contact";
 import About from "./pages/About";
 import Auth from "./pages/Auth";
 import Account from "./pages/Account";
+import { ConsentBanner } from "./components/tracking/ConsentBanner";
+import { trackingService } from "./services/trackingService";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
+const App = () => {
+  const [consentGiven, setConsentGiven] = useState(false);
+
+  useEffect(() => {
+    // Check if consent was already given
+    const consent = localStorage.getItem('eclat-consent');
+    if (consent) {
+      setConsentGiven(true);
+      initializeTracking(JSON.parse(consent));
+    }
+  }, []);
+
+  const initializeTracking = async (consents: { location: boolean; analytics: boolean }) => {
+    if (consents.analytics) {
+      await trackingService.trackUserSession({
+        requestLocation: consents.location,
+        pageUrl: window.location.href,
+        referrer: document.referrer,
+      });
+    }
+  };
+
+  const handleConsentGiven = (consents: { location: boolean; analytics: boolean }) => {
+    setConsentGiven(true);
+    initializeTracking(consents);
+  };
+
+  return (
+    <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/shop" element={<Shop />} />
-            <Route path="/product/:id" element={<ProductDetail />} />
-            <Route path="/collections" element={<Shop />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/size-guide" element={<SizeGuide />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/wishlist" element={<Wishlist />} />
-            <Route path="/auth" element={<Auth />} />
-            <Route path="/account" element={<Account />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Layout>
+        <TooltipProvider>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/shop" element={<Shop />} />
+              <Route path="/product/:id" element={<ProductDetail />} />
+              <Route path="/size-guide" element={<SizeGuide />} />
+              <Route path="/wishlist" element={<Wishlist />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/auth" element={<Auth />} />
+              <Route path="/account" element={<Account />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Layout>
+          <Toaster />
+          <Sonner />
+          {!consentGiven && <ConsentBanner onConsentGiven={handleConsentGiven} />}
+        </TooltipProvider>
       </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+    </QueryClientProvider>
+  );
+};
 
 export default App;
+                                         
